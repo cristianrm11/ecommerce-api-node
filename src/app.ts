c8407dcfb529340f2a29946c6fea88f2147ea026
cristian <cristian@rightpedal.io>
2024-11-05 14:36:13 -0600

    add missing types for express and create app ts to manage the routes for producs and orders

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response}  from 'express'
+import dotenv from 'dotenv';
+
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/running', (req: Request, res: Response) => {
+    res.status(200).json({status: 'Ok'})
+})
+
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('test')
+import app from './app'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})

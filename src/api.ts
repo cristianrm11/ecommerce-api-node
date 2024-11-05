2164b2638619973b9d8efe9fa958f2144ee24609
cristian <cristian@rightpedal.io>
2024-11-05 14:39:35 -0600

    typo rename api file

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,6 @@
 import express, {Request, Response}  from 'express'
 import dotenv from 'dotenv';
 
-
 dotenv.config()
 
 const app = express()
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import app from './app'
+import api from './api'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+api.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
